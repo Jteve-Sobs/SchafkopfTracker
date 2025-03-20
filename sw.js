30fe6ab5df2a0efc130d06c6894b82530b958f06
Florian Reichert <46194843+Jteve-Sobs@users.noreply.github.com>
2025-03-20 23:22:17 +0100

    Confirmation Dialoge für Löschen hinzufügen, ServiceWorker fixen (halleluuja), Version bump

--- sw.js
+++ sw.js
@@ -12,8 +12,6 @@ async function getVersion() {
 }
 
 const CACHE_NAME_PREFIX = "my-app-cache-";
-let CACHE_NAME = CACHE_NAME_PREFIX + "default"; // Temporary value before fetching version
-
 const urlsToCache = [
   "index.html",
   "chart.js",
@@ -25,46 +23,40 @@ const urlsToCache = [
 // Install Event: Cache Files with Versioned Cache Name
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    getVersion().then((version) => {
-      CACHE_NAME = `${CACHE_NAME_PREFIX}${version}`;
-      return caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache));
-    })
+    (async () => {
+      const version = await getVersion();
+      const CACHE_NAME = CACHE_NAME_PREFIX + version;
+      console.log("Installing service worker with cache name:", CACHE_NAME);
+
+      const cache = await caches.open(CACHE_NAME);
+      await cache.addAll(urlsToCache);
+
+      // Save the cache name for later use in the activate event
+      self.CACHE_NAME = CACHE_NAME;
+    })()
   );
-  self.skipWaiting();
+  self.skipWaiting(); // Force immediate activation of the new SW
 });
 
 // Activate Event: Delete Old Caches
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    getVersion().then((version) => {
-      const expectedCacheName = `${CACHE_NAME_PREFIX}${version}`;
-      return caches.keys().then((cacheNames) => {
-        return Promise.all(
-          cacheNames.map((cache) => {
-            if (cache !== expectedCacheName) {
-              return caches.delete(cache);
-            }
-          })
-        );
-      });
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          // .filter((name) => name !== self.CACHE_NAME) // don't delete this line. otherwise, it will not delete the currently used cache. no thanks to ChatGPT
+          .map((name) => caches.delete(name))
+      );
     })
   );
-  self.clients.claim();
+  self.clients.claim(); // Force immediate control over all clients
 });
 
 // Fetch Event: Serve from Cache or Fetch from Network
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return (
-        response ||
-        fetch(event.request).then((fetchResponse) => {
-          return caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, fetchResponse.clone());
-            return fetchResponse;
-          });
-        })
-      );
+      return response || fetch(event.request);
     })
   );
 });
